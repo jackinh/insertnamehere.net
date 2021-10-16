fb86c5a75e90bbf76332b98958989c9901f7f645
Jack Reggin <jack.reggin@gmail.com>
2021-10-16 18:39:21 +0200

    initial pull from tina-cloud-starter

--- wrap-with-realm-provider.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import RealmProvider from './src/services/mongodb-realm-provider'
-
-export default function WrapWithRealmProvider({ element }) {
-    return <RealmProvider appId={process.env.REALM_APP_ID}>{element}</RealmProvider>;
-}
